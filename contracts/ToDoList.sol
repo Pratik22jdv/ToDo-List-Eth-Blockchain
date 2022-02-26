46cb6d2fdd5546a0e5eaf3d9bb78ebac53eff209
Pratik22jdv <prjadhav2209@gmail.com>
2022-02-26 10:11:50 +0530

    4. Final commit

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -12,6 +12,17 @@ contract ToDoList
 
     mapping(uint=>Task) public tasks; //Storing key value pair
 
+    event TaskCreated (
+        uint id, 
+        string content, 
+        bool completed
+    ); //To listen to external event on client side
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
     //Add default task in to-do list when contract is deployed
     constructor () public {
         createTask("Finish To-Do-List Blockchain project");
@@ -21,8 +32,16 @@ contract ToDoList
     {
         taskCount++;
         tasks[taskCount]=Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
     //tasks[] has an dynamic size and there is no way to know the size of this hashMap.
     //We can't iterate over tasks[] and all content in it. We need to have reference to fetch particular content.
     //That's why mapping is used and taskCount variable is maintained.
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+  }
 }
